35a4ef202caec0ab1fad4e41801d5300dddd0018
amiramitai <amiramitai@users.noreply.github.com>
2021-05-13 21:47:04 +0300

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,52 +1,53 @@
 function code() {
     # locate the vscode remote server and open it from the client window 
-	CODE=$(find ~/.vscode-server/bin/ -name "code" -printf "%T@ %Tc %p\n" | sort -n | tail -n 1 | awk '{print $NF}')
-	VSCODE_IPC_HOOK_CLI=$(netstat -an | grep -Eo "/.*/vscode-ipc.*\.sock" | head -n 1)
-	VSCODE_IPC_HOOK_CLI=$VSCODE_IPC_HOOK_CLI $CODE -g $@
+    CODE=$(find ~/.vscode-server/bin/ -name "code" -printf "%T@ %Tc %p\n" | sort -n | tail -n 1 | awk '{print $NF}')
+    VSCODE_IPC_HOOK_CLI=$(netstat -an | grep -Eo "/.*/vscode-ipc.*\.sock" | head -n 1)
+    VSCODE_IPC_HOOK_CLI=$VSCODE_IPC_HOOK_CLI $CODE -g $@
 }
 
 function live_grep() {
-    # this function would live grep using fzf. 
+    # live grep using fzf.
+    # To launch: ctrl+g
     # press enter to select current filepath
-    # ctrl+g re-grep result with fzf
-    # ctrl+s to open with vscode
-    # ctrl+v to open with vim
-	local tmpfile=$(mktemp /tmp/livegrep.data.XXXXXX)
-	local editorfile=$(mktemp /tmp/livegrep.editor.XXXXXX)
-	local selected=$(printf '' | fzf --ansi --header='Type to Live-grep' --phony --no-height \
-									--bind "change:execute-silent(rg -i --color=always --vimgrep {q} > ${tmpfile})+reload(cat ${tmpfile})" \
-									--bind "ctrl-v:execute-silent(printf 'vim' > $editorfile)+accept" \
-									--bind "ctrl-g:execute-silent(printf 'fzf' > $editorfile)+accept" \
-									--bind "ctrl-s:execute-silent(printf 'code' > $editorfile)+accept")
-	local editor=$(cat $editorfile)
-	case "$editor" in
-		"fzf")
-			rm -rf $editorfile
-			selected=$(cat $tmpfile | fzf --ansi \
-				--bind "ctrl-v:execute-silent(printf 'vim' > $editorfile)+accept" \
-				--bind "ctrl-s:execute-silent(printf 'code' > $editorfile)+accept");
-		;;
-		*)
-	esac
-	editor=$(cat $editorfile)
-	printf $selected > /tmp/s
-	rm -rf $tmpfile
-	rm -rf $editorfile
-	filepath=$(realpath $(printf $selected | cut -d ":" -f1))
-	line=$(echo $selected | cut -d ":" -f2)
-	char=$(echo $selected | cut -d ":" -f3)
-	case "$editor" in
-		"vim")
-			zsh -c "vim $filepath +$line < $TTY"
-		;;
-		"code")
-			code $filepath:$line:$char
-		;;
-		*)
-			LBUFFER=$LBUFFER$filepath:$line:$char
-	esac
-	zle reset-prompt
-	zle redisplay
+    # press ctrl+g re-grep result with fzf
+    # press ctrl+s to open with vscode
+    # press ctrl+v to open with vim
+    local tmpfile=$(mktemp /tmp/livegrep.data.XXXXXX)
+    local editorfile=$(mktemp /tmp/livegrep.editor.XXXXXX)
+    local selected=$(printf '' | fzf --ansi --header='Type to Live-grep' --phony --no-height \
+                                     --bind "change:execute-silent(rg -i --color=always --vimgrep {q} > ${tmpfile})+reload(cat ${tmpfile})" \
+                                     --bind "ctrl-v:execute-silent(printf 'vim' > $editorfile)+accept" \
+                                     --bind "ctrl-g:execute-silent(printf 'fzf' > $editorfile)+accept" \
+                                     --bind "ctrl-s:execute-silent(printf 'code' > $editorfile)+accept")
+    local editor=$(cat $editorfile)
+    case "$editor" in
+        "fzf")
+            rm -rf $editorfile
+            selected=$(cat $tmpfile | fzf --ansi \
+                                          --bind "ctrl-v:execute-silent(printf 'vim' > $editorfile)+accept" \
+                                          --bind "ctrl-s:execute-silent(printf 'code' > $editorfile)+accept");
+        ;;
+        *)
+    esac
+    editor=$(cat $editorfile)
+    printf $selected > /tmp/s
+    rm -rf $tmpfile
+    rm -rf $editorfile
+    filepath=$(realpath $(printf $selected | cut -d ":" -f1))
+    line=$(echo $selected | cut -d ":" -f2)
+    char=$(echo $selected | cut -d ":" -f3)
+    case "$editor" in
+        "vim")
+            zsh -c "vim $filepath +$line < $TTY"
+        ;;
+        "code")
+            code $filepath:$line:$char
+        ;;
+        *)
+            LBUFFER=$LBUFFER$filepath:$line:$char
+    esac
+    zle reset-prompt
+    zle redisplay
 }
 
 function kill_proccess() {
@@ -56,8 +57,8 @@ function kill_proccess() {
 
 function up() {
     # travels one directory up
-	cd ..
-	BUFFER=""
+    cd ..
+    BUFFER=""
     zle accept-line
 }
 
