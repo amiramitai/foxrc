e1175bf2e4c943f2462e1c97d9a7527ed4ca74da
amiramitai <amiramitai@users.noreply.github.com>
2021-05-26 15:27:41 +0300

    Update zsh_setup.sh

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -6,25 +6,28 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's#ZSH_THEME=\".*\"#ZSH_THEME=\"powerlevel10k/powerlevel10k\"#g' ~/.zshrc
 
+# install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# install oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 sed -i 's/plugins=(.*)/plugins=(git zsh-autosuggestions zsh-completions zsh-syntax-highlighting)/g' ~/.zshrc
 
+# install foxrc
 git clone https://github.com/amiramitai/foxrc.git ~/foxrc
 echo 'source ~/foxrc/dev.sh' >> ~/.zshrc
 
+# install missing settings for fzf
 echo 'export LC_ALL=en_US.UTF-8' >> ~/.zshrc
 echo 'export LANG=en_US.UTF-8' >> ~/.zshrc
 
+# install grc
 git clone https://github.com/garabik/grc /tmp/grc
 cd /tmp/grc
 sh install.sh
-
 mkdir -p ~/.grc
 cp ~/foxrc/grc.conf ~/.grc/auto
 echo "alias grc='grc -s -e -c auto'" >> ~/.zshrc
